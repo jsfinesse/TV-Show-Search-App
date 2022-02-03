709436cade18f09d372ac714367640cae7ab9e37
Prithu Singh <prithusingh0327@gmail.com>
2022-02-03 13:40:01 +0530

    Added styles

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const form = document.querySelector('#searchForm');
-const shelf = document.querySelector('.shelf');
+const form = document.querySelector("#searchForm");
+const shelf = document.querySelector(".shelf");
 
-form.addEventListener('submit', async function(e) {
+form.addEventListener("submit", async function (e) {
     e.preventDefault();
     deleteImages();
     const searchTerm = form.elements.query.value;
-    const config = { params : {q: searchTerm}};
+    const config = { params: { q: searchTerm } };
     const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
     // console.log(res.data[0].show.url);
     displayImages(res.data);
-    form.elements.query.value = '';
-})
+    form.elements.query.value = "";
+});
 
 const displayImages = (shows) => {
-    for(let result of shows) {
-        if(result.show.image) {
-            const link = document.createElement('a');
+    for (let result of shows) {
+        if (result.show.image) {
+            const link = document.createElement("a");
             link.href = result.show.url;
-            link.target = '_blank';
-            const img = document.createElement('img');
+            link.target = "_blank";
+            const img = document.createElement("img");
             img.src = result.show.image.medium;
             link.appendChild(img);
             shelf.appendChild(link);
         }
     }
-}
+};
 
 const deleteImages = () => {
-    const imgs = document.querySelectorAll('img');
-    if(imgs.length === 0) return;
-    for(let img of imgs) {
+    const imgs = document.querySelectorAll("img");
+    if (imgs.length === 0) return;
+    for (let img of imgs) {
         img.remove();
     }
-}
+};
